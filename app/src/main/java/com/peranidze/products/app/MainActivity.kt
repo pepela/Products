f90615e8d3bd3327bc26a5ee17787570f5e5cefb
Giorgi Peranidze <giorgi.peranidze@gmail.com>
2020-09-03 10:08:17 +0300

    UI changes
        * add layout for product detail fragment
        * change layout for product list items
        * add day/night colors and style
        * add launcher screen theme

--- app/src/main/java/com/peranidze/products/app/MainActivity.kt
+++ app/src/main/java/com/peranidze/products/app/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
     }
