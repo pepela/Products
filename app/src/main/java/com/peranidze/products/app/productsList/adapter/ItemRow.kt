642703869311ca3de9077cfd83034c8a328aa0a5
Giorgi Peranidze <giorgi.peranidze@gmail.com>
2020-09-16 17:47:55 +0300

    Parcel bug
        * fix crash by adding parcelize to item classes

--- app/src/main/java/com/peranidze/products/app/productsList/adapter/ItemRow.kt
+++ app/src/main/java/com/peranidze/products/app/productsList/adapter/ItemRow.kt
@@ -1,9 +1,14 @@
 package com.peranidze.products.app.productsList.adapter
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 sealed class ItemRow(val itemType: ItemType) {
 
-    data class CategoryItem(val name: String) : ItemRow(ItemType.CATEGORY)
+    @Parcelize
+    data class CategoryItem(val name: String) : ItemRow(ItemType.CATEGORY), Parcelable
 
+    @Parcelize
     data class ProductItem(
         val id: Long,
         val categoryId: Long,
@@ -12,7 +17,7 @@ sealed class ItemRow(val itemType: ItemType) {
         val imageUrl: String,
         val price: String,
         val currency: String
-    ) : ItemRow(ItemType.PRODUCT)
+    ) : ItemRow(ItemType.PRODUCT), Parcelable
 
     fun getSharedElementId() = hashCode()
 }
